5e95ebbfb8b8af255e926edc33ca53e677bd959c
akmaldira <akmaldiraa@gmail.com>
2022-10-02 21:08:27 +0700

    Initial Commit

---
+++ functions/simEndpoint.js
@@ -0,0 +1,55 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const login = async (npm, password) => {
+  const url = "https://simkuliah.unsyiah.ac.id/index.php";
+  const cookie = await axios.get(url)
+  .then((res) => {
+    return res.headers['set-cookie'][0].split('; ')[0];
+  })
+  const body = new URLSearchParams({
+    username: npm,
+    password: password,
+  })
+  const hitLogin = await axios.post(`${url}/login`, 
+    body,
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        cookie: cookie,
+      },
+    })
+  .then((res) => {
+    return res.data;
+  })
+
+  const $ = cheerio.load(hitLogin);
+  const name = $('.user-profile a span').text();
+  const typeOfUser = $('.nav-right li .label-danger').text();
+  if (name == '') {
+    return null
+  } else {
+    return {
+      name,
+      typeOfUser,
+    };
+  }
+}
+
+exports.handler = async (event, handler) => {
+  // const { npm, password } = JSON.parse(event.body);
+  const { npm, password } = event.queryStringParameters;
+  const result = await login(npm, password);
+  if (result == null) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({
+        message: "Failed Login"
+      })
+    }
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result)
+  }
+}
